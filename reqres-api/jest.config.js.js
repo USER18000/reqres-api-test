1f3c7696f76b1a4c9364ba3af4c775f7c6ca3b40
Max Mironov <mexc35633@gmail.com>
2025-11-27 03:50:58 +0300

    Update jest.config.js.js

--- reqres-api/jest.config.js.js
+++ reqres-api/jest.config.js.js
@@ -1,13 +1,10 @@
 // jest.config.js
 
 module.exports = {
-  // Убираем testRunner, если он не нужен для кастомного окружения (часто используется стандартный)
-  // testRunner: 'jest-circus/runner', // <-- Закомментируйте или удалите эту строку
+  
+  testEnvironment: "allure-jest/node", 
 
-  // Указываем кастомное окружение из allure-jest
-  testEnvironment: "allure-jest/node", // <--- Вот тут изменение
-
-  // setupFilesAfterEnv: ['allure-jest/node'], // <-- Закомментируйте или удалите эту строку
+  
 
   transform: {
     '^.+\\.[tj]s$': ['babel-jest', { configFile: './babel.config.js' }]
