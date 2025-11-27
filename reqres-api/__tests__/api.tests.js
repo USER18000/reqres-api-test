e17ab8805dfff6ae5e1b82b2157958445efc0ef0
Max Mironov <mexc35633@gmail.com>
2025-11-27 03:48:10 +0300

    Update api.tests.js

--- reqres-api/__tests__/api.tests.js
+++ reqres-api/__tests__/api.tests.js
@@ -6,13 +6,13 @@ describe('ReqRes API Tests (with API Key)', () => {
   it('should return list of users with status 200', async () => {
     const response = await axios.get('https://reqres.in/api/users', {
       headers: {
-        'x-api-key': 'reqres-free-v1' // ← вот он! Твой ключ
+        'x-api-key': 'reqres-free-v1' 
       }
     });
 
     expect(response.status).toBe(200);
     expect(response.data).toHaveProperty('data');
     expect(Array.isArray(response.data.data)).toBe(true);
-    expect(response.data.data.length).toBe(6); // как в знаниях
+    expect(response.data.data.length).toBe(6); 
   });
 });
